fe504bac4b6771c36ebf99da668bffb91d985382
Lorraine <lorrainecannom@gmail.com>
2023-01-22 20:28:15 -0500

    containerizing

--- user-service/src/main/resources/data.sql
+++ user-service/src/main/resources/data.sql
@@ -2,3 +2,8 @@ INSERT INTO roles(insert_date_time, insert_user_id, is_deleted, last_update_date
 VALUES ('2022-01-05 00:00:00', 1, false, '2022-01-05 00:00:00', 1, 'Admin'),
        ('2022-01-05 00:00:00', 1, false, '2022-01-05 00:00:00', 1, 'Manager'),
        ('2022-01-05 00:00:00', 1, false, '2022-01-05 00:00:00', 1, 'Employee');
+
+-- insert into users(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id, enabled,
+--                   first_name, gender, last_name, phone, user_name, role_id,pass_word)
+-- values ('2022-01-05 00:00:00', 1, false, '2022-01-05 00:00:00', 1, true, 'Lorraine', 'MALE', 'Cannom', '5198788875','admin@admin.com',
+--         1,'$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK');
