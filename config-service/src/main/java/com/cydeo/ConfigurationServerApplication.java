239441cb8e166ed7725055ac5ada2580c3ba91f0
Lorraine <lorrainecannom@gmail.com>
2023-01-29 14:32:18 -0500

    Added GitHub configurations

--- config-service/src/main/java/com/cydeo/ConfigurationServerApplication.java
+++ config-service/src/main/java/com/cydeo/ConfigurationServerApplication.java
@@ -2,7 +2,7 @@ package com.cydeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 
 @SpringBootApplication
